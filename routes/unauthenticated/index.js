ccf1a0b7c116126fba7716ab46aca06b851443c7
Jo Ey <cloud.joey98@gmail.com>
2021-05-08 16:11:38 +0800

    logged in user now correctly redirected to dashboard straight if they access '/'

--- routes/unauthenticated/index.js
+++ routes/unauthenticated/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(req.isAuthenticated()) {
+    res.redirect('/dashboard');
+  }
   res.render('index', { });
 });
 

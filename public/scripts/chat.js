3394e24b45ea66642c4683bc2edd3d50dbeb2017
Adam Belaid <adam.belaid7@gmail.com>
2021-05-16 11:59:25 -0700

    separated lobby and chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -21,28 +21,5 @@ socket.on('newMessage', function(msg) {
 	messages.appendChild(item);
 });
 
-var gameForm = document.getElementById('gameForm');
-
-async function getResponse(url, next) {
-  let res = await fetch(url);
-  let jason = await res.json();
-  next(jason);
-}
-
-gameForm.addEventListener('submit', function(e) {
-	e.preventDefault();
-	getResponse("/game/createGame", function(jason) {
-		socket.emit('enterGame', jason);
-	});
-});
-
-var pageForm = document.getElementById('pageForm');
-
-socket.on('enterGame', function(game_id) {
-	var url = "/game/g/"+game_id+"/";
-	pageForm.action = url;
-	pageForm.submit();
-});
-
 
 
